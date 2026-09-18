ab467ff4f4fa2f6536e0ee86722a4899067dc49e
johnliesercox <john.liesercox@gmail.com>
2026-09-18 08:57:16 +0100

    Season one now opens on "Play the match".

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Soccer Manager '91 service worker: cache the app shell so it works offline and installs to the home screen.
-const VERSION = 'sm91-v1.0.1';
+const VERSION = 'sm91-v1.0.3';
 const SHELL = ['./', './index.html', './manifest.webmanifest', './icon-192.png', './icon-512.png'];
 self.addEventListener('install', e => { e.waitUntil(caches.open(VERSION).then(c => c.addAll(SHELL)).then(() => self.skipWaiting())); });
 self.addEventListener('activate', e => { e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== VERSION).map(k => caches.delete(k)))).then(() => self.clients.claim())); });
